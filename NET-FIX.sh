5a3e29774646ed6d95f9cbd945d676988c6f28b5
CortezJEL <53353250+CortezJEL@users.noreply.github.com>
2021-03-07 18:15:34 -0500

    Update NET-FIX.sh

--- NET-FIX.sh
+++ NET-FIX.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-airmon-ng stop wlan0mon
-airmon-ng stop wlan1mon
-airmon-ng stop wlan2mon
-airmon-ng stop wlan3mon
-airmon-ng stop wlan4mon
-airmon-ng stop wlan5mon
-airmon-ng stop wlan6mon
-airmon-ng stop wlan7mon
-airmon-ng stop wlan8mon
-airmon-ng stop wlan9mon
-airmon-ng stop mon0
-airmon-ng stop mon1
-airmon-ng stop mon2
-airmon-ng stop mon3
-airmon-ng stop mon4
-airmon-ng stop mon5
-airmon-ng stop mon6
-airmon-ng stop mon7
-airmon-ng stop mon8
-airmon-ng stop mon9
-service networking restart
-service network-manager restart
-service Networking restart
-service NetworkManager restart
+airmon-ng stop wlan0mon 2>&1 >/dev/null
+airmon-ng stop wlan1mon 2>&1 >/dev/null
+airmon-ng stop wlan2mon 2>&1 >/dev/null
+airmon-ng stop wlan3mon 2>&1 >/dev/null
+airmon-ng stop wlan4mon 2>&1 >/dev/null
+airmon-ng stop wlan5mon 2>&1 >/dev/null
+airmon-ng stop wlan6mon 2>&1 >/dev/null
+airmon-ng stop wlan7mon 2>&1 >/dev/null
+airmon-ng stop wlan8mon 2>&1 >/dev/null
+airmon-ng stop wlan9mon 2>&1 >/dev/null
+airmon-ng stop mon0 2>&1 >/dev/null
+airmon-ng stop mon1 2>&1 >/dev/null
+airmon-ng stop mon2 2>&1 >/dev/null
+airmon-ng stop mon3 2>&1 >/dev/null
+airmon-ng stop mon4 2>&1 >/dev/null
+airmon-ng stop mon5 2>&1 >/dev/null
+airmon-ng stop mon6 2>&1 >/dev/null
+airmon-ng stop mon7 2>&1 >/dev/null
+airmon-ng stop mon8 2>&1 >/dev/null
+airmon-ng stop mon9 2>&1 >/dev/null
+service networking restart 2>&1 >/dev/null
+service network-manager restart 2>&1 >/dev/null
+service Networking restart 2>&1 >/dev/null
+service NetworkManager restart 2>&1 >/dev/null
 echo " Network fixed from monitor mode not allowing connection "
 read -p "Press enter to exit"
